9fca02e080f81621ff13e6ca75feee5171a4a1b5
Elton Freires <elton.freires@gmail.com>
2023-09-13 18:51:55 -0300

    primeiro teste

--- src/main/java/org/example/rest/OlaMundo.java
+++ src/main/java/org/example/rest/OlaMundo.java
@@ -3,13 +3,19 @@ package org.example.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
     public static void main(String[] args) {
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
         System.out.println(response.getBody().asString());
-        response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola?key=123");
         System.out.println(response.statusCode());
+
+        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+        System.out.println(response.statusCode() == 200);
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
     }
 
     public void solucoesProblemas() {
