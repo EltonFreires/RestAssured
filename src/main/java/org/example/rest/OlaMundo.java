3491cb0834cfa8cef3ae06ff9935166283a2a00d
Elton Freires <elton.freires@gmail.com>
2023-09-13 17:52:30 -0300

    status code

--- src/main/java/org/example/rest/OlaMundo.java
+++ src/main/java/org/example/rest/OlaMundo.java
@@ -8,5 +8,16 @@ public class OlaMundo {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
         System.out.println(response.getBody().asString());
+        response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola?key=123");
+        System.out.println(response.statusCode());
+    }
+
+    public void solucoesProblemas() {
+        // caso ocorra erro de acesso no modelo abaixo
+        // Response response = RestAssured.request(Method.GET, "https://localhost/api");
+        // Possíveis soluções
+        // Response response = RestAssured.request(Method.GET, "https://localhost:80/api");
+        // ping no servidor "ping localhost", depois coloca o endereço
+        //      Response response = RestAssured.request(Method.GET, "https://127.0.0.1/api");
     }
 }
