d58b24ff50ac1d658137d3d3f23ded21fa8a67d8
Elton Freires <elton.freires@gmail.com>
2023-09-14 14:44:39 -0300

    Modo fluente

--- src/main/java/org/example/rest/OlaMundoTeste.java
+++ src/main/java/org/example/rest/OlaMundoTeste.java
@@ -7,12 +7,12 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class OlaMundoTeste {
-    Response response;
+import static io.restassured.RestAssured.*;
 
+public class OlaMundoTeste {
     @Test
-    public void OlaMundo() {
-        response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+    public void OlaMundo1() {
+        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
         Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         Assert.assertTrue(response.statusCode() == 200);
 
@@ -21,27 +21,42 @@ public class OlaMundoTeste {
     }
 
     @Test
-    // Falha: erro na validação
+    public void OlaMundo2() {
+        get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+    }
+
+    @Test
+    public void OlaMundo3() {
+        // Given/When/Then
+        given()
+        .when()
+            .get("http://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200);
+    }
+
+    @Test
     public void ExFalha01() {
+        // Falha: erro na validação
         System.out.println("2 falhas, e não diz o aconteceu, só aponta onde ocorreu o erro!");
         Assert.assertTrue(200 == 201);
         Assert.assertTrue("200".equals("201"));
     }
 
     @Test
-    // Falha: erro na validação
     public void ExFalha02() {
+        // Falha: erro na validação
         System.out.println("2 falhas, informando manualmente o que aconteceu!");
         int statusEsperado = 200;
         int statusRetornado = 201;
-        Assert.assertTrue("Status esperado:"+statusEsperado+", Status retornado:"+ statusRetornado, 200 == 201);
+        Assert.assertTrue("Status esperado:" + statusEsperado + ", Status retornado:" + statusRetornado, 200 == 201);
     }
 
     @Test
-    // Falha: erro na validação
     public void ExFalha03() {
+        // Falha: erro na validação
         System.out.println("2 erros, informando automaticamente o que aconteceu!");
-        Assert.assertEquals(200,201);
+        Assert.assertEquals(200, 201);
     }
 
     @Test
