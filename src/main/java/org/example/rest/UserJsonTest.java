2a263808bdcefbd9fc08bcd11b8350d1ffa2d843
Elton Freires <elton.freires@gmail.com>
2023-09-16 12:20:09 -0300

    Aula 13, Json segundo nível com lista

--- src/main/java/org/example/rest/UserJsonTest.java
+++ src/main/java/org/example/rest/UserJsonTest.java
@@ -51,4 +51,19 @@ public class UserJsonTest {
                 .body("name", containsString("Joaquina"))
                 .body("endereco.rua", is("Rua dos bobos"));
     }
+
+    @Test
+    public void verificandoSegundoNivelLista(){
+        given()
+                .when()
+                .get(pathApi+"/users/3")
+
+                .then()
+                .statusCode(200)
+                .body("name", containsString("Ana"))
+                .body("filhos", hasSize(2))
+                .body("filhos[0].name", is("Zezinho"))
+                .body("filhos[1].name", is("Luizinho"))
+                .body("filhos.name", hasItem("Luizinho"));
+    }
 }
